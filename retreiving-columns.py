8d3027c16b33a893f670e48bd4ac3320d3eca2dc
ahmedrazarais <razarais28@gmail.com>
2024-04-08 07:24:47 +0500

    Retreiving columns

---
+++ retreiving-columns.py
@@ -0,0 +1,59 @@
+# Write a SQL query to retrieve all columns from a table named employees
+
+
+
+
+# firt import sql connector abd password username from config.py file
+import mysql.connector
+from config import DB_HOST, DB_USER, DB_PASSWORD
+
+# making a class
+class Getting_tables:
+    def __init__(self):
+        try:
+            # MAKING A CONNECTION WITH MYSQL
+            self.connection=mysql.connector.connect(
+                host=DB_HOST,
+                user=DB_USER,
+                password=DB_PASSWORD
+               
+            )
+            # MAKE A POINTER FOR EXECUTING QUERY
+            self.cursor=self.connection.cursor()
+            
+            
+
+            print("Connection created successfull")
+        # IF CONNECTION NOT MADE SUCCESSFULLY
+        except mysql.connector.Error as e:
+            print(f"Error in connection {e}")
+    
+
+    # making a mehtod to make update
+    def update_process(self):
+        try:
+           # ASSUMING HAVE TABLE NAME EMPLOYEES
+           self.cursor.execute("SELECT * FROM employees")
+           rows=self.cursor.fetchall()
+           print(rows)
+        except mysql.connector.Error as e:
+            print(f"Error in connection {e}")
+        
+    def close_connection(self):
+        # Close database connection
+        try:
+            if self.connection.is_connected():
+                self.cursor.close()
+                self.connection.close()
+                print("Connection to MySQL database closed.")
+        except mysql.connector.Error as e:
+            print(f"Error closing connection: {e}")
+
+
+
+    
+
+# MAKE INSTANCE OF CLASS
+executor=Getting_tables()
+# CALL HERE MEHTOD
+executor.close_connection()
